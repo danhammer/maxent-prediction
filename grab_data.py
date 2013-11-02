feb6de020f4bb24f3a0396178c6f0387be04fce3
Dan Hammer <dan.s.hammer@gmail.com>
2013-11-02 13:08:41 -0700

    added search and plot functions

--- grab_data.py
+++ grab_data.py
@@ -3,16 +3,47 @@ import requests
 import json
 import pandas as pd
 
-def grab_series(series_id = "DJIA", start="2013-04-01", end="2013-05-01"):
+def grab_series(series_id = "DJIA", start = "1970-01-01"):
     base = "http://api.stlouisfed.org/fred/series/observations"
     series = "?series_id=%s" % series_id
-    api_key = os.environ["FED_API_KEY"]
-    url = "%s%s&api_key=%s&file_type=json" % (base, series, api_key)
+    api_key = "&api_key=%s" % os.environ["FED_API_KEY"]
+    date = "&observation_start=%s" % start
+    url = "%s%s%s%s&file_type=json" % (base, series, date, api_key)
     r =  requests.get(url).content
     obs = json.loads(r)
     return pd.DataFrame(obs['observations'])
     
     
+def plot_series(series_id = "UNRATENSA"):
+    data = grab_series(series_id)
+    def convert(date):
+        return datetime.datetime.strptime(date, '%Y-%m-%d')
     
+    data[['date']] = data[['date']].applymap(convert)
+    data = data.set_index('date')
+
+    # filter out null values
+    data = data[data.value != "."]
+
+    data['value'].plot()
+    plt.show()
+    return data
 
 
+def search_fred(*terms):
+    base = "http://api.stlouisfed.org/fred/series/search"
+    search = "?search_text=%s" % "+".join(terms)
+    api_key = "&api_key=%s" % os.environ["FED_API_KEY"]
+    url = "%s%s%s&file_type=json" % (base, search, api_key)
+    r =  requests.get(url).content
+    obs = json.loads(r)
+    
+    def _process(d):
+        return {'id': d['id'], 'title': d['title'], 'freq':d['frequency']}
+
+    return map(_process, obs['seriess'])
+
+def print_search(n = 10, *terms):
+    res = search_fred(*terms)
+    for x in res[0:n]:
+        print "%s: %s (%s)" %(x['id'], x['title'], x['freq'])
